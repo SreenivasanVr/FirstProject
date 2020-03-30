48488b6790c3cf02097e746084c8894780027f7d
xsvn9 <xsvn9@EQPLSTDP10070.whs.dell.local>
2020-03-31 10:35:09 +1100

    First Commit

---
+++ POCProject/src/First/FirstClass.java
@@ -0,0 +1,12 @@
+package First;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("First Git Hub Project Setup");
+		
+	}
+
+}
